4a1f6b0068423b99c66826ee672d78f1f08b2425
dhwarakesh <dhwarakesh31@gmail.com>
2024-11-25 14:19:27 -0500

    fixed quotes errors

--- main.tf
+++ main.tf
@@ -3,6 +3,6 @@ module "tool-automation" {
   source = "./module"
 
   tool_name = each.key
-  instance_type = each.value[instance_type]
+  instance_type = each.value["instance_type"]
   zone_id = var.zone_id
 }
