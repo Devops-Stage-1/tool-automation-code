def041021b37fae8f21ae7c3293a99d2f4a0dda4
dhwarakesh <dhwarakesh31@gmail.com>
2024-11-27 16:19:40 -0500

    Created IAM role with inline policy and instance profile.

--- main.tf
+++ main.tf
@@ -5,4 +5,5 @@ module "tool-automation" {
   tool_name = each.key
   instance_type = each.value["instance_type"]
   zone_id = var.zone_id
+  policy_resource_list = each.value["policy_resource_list"]
 }
